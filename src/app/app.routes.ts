de5d6109e4aa9019c30c5aed91fc73b65a20a2e1
Isabelle Braz <pih_braz@hotmail.com>
2025-07-22 20:10:59 -0300

    add pages admin/painel e user/perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,55 @@
 import { Routes } from '@angular/router';
+
+
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+
+import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
+
+import { AdminPackagesComponent } from './pages/admin/admin-packages/admin-packages.component';
+import { AdminUsersComponent } from './pages/admin/admin-users/admin-users.component';
+import { AdminReviewsComponent } from './pages/admin/admin-reviews/admin-reviews.component';
+import { AdminReportsComponent } from './pages/admin/admin-reports/admin-reports.component';
 import { DefaultHomeComponent } from './pages/home/default-home/defaultHome.component';
-
-
+import { UserProfileComponent } from './pages/profile/user-profile/user-profile.component';
 
 export const routes: Routes = [
+ 
+  {
+    path: "",
+    component: DefaultHomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  },
+  {
+    path: "forgot-password",
+    component: ForgotPasswordComponent
+  },
+  {
+    path: 'perfil', 
+    component: UserProfileComponent
 
-    {
-        path: "",
-        component: DefaultHomeComponent
-    },
-    {
-        path: "login",
-        component: LoginComponent
+  },
 
-    },
-    {
-
-        path: "signup",
-        component: SignupComponent
-    },
-    {
-        path: "forgot-password",
-        component: ForgotPasswordComponent
-
-    }
-
-
-
+  {
+    path: 'admin',
+    component: AdminLayoutComponent, 
+    // Rota do Painel Administrativo
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+       
+      { path: 'dashboard', component: AdminUsersComponent },
+      { path: 'pacotes', component: AdminPackagesComponent },
+      { path: 'usuarios', component: AdminUsersComponent },
+      { path: 'avaliacoes', component: AdminReviewsComponent },
+      { path: 'relatorios', component: AdminReportsComponent },
+    ]
+  }
 ];
---
+++ src/app/pages/admin/admin-packages/admin-packages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-packages',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-packages.component.html',
+  styleUrl: './admin-packages.component.scss'
+})
+export class AdminPackagesComponent {
+
+}
