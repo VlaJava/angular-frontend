d2cb4889f1b5874390997a7d7b8ec35060fda329
filipe4ndrade <filipeandrade000@gmail.com>
2025-07-31 04:07:23 -0300

    feat/message.component

---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChatMessage, Sender } from '../../types/travel.types';
+import { PackageCardComponent } from '../package-card-2/package-card.component';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [CommonModule, PackageCardComponent],
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.scss']
+})
+export class MessageComponent {
+  @Input() message!: ChatMessage;
+
+  get isBot(): boolean {
+    return this.message.sender === Sender.Bot;
+  }
+}
