80dae40ab322183d85c1cbf0c9ecbf3e0a7776bb
filipe4ndrade <filipeandrade000@gmail.com>
2025-08-07 13:39:46 -0300

    feat/ adiciao do tradutor no footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoogleTranslateService } from '../../services/google-translate.service'; 
+
+declare const google: any;
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    imports: [],
-    templateUrl: './footer.component.html',
-    styleUrl: './footer.component.scss'
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  constructor(private googleTranslateService: GoogleTranslateService) { }
+
+  ngOnInit(): void {
+  
+    (window as any).googleTranslateElementInit = () => {
+      new google.translate.TranslateElement({
+        pageLanguage: 'pt',
+        includedLanguages: 'en,es,pt',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false
+      }, 'google_translate_element_footer'); 
+    };
+
+
+    this.googleTranslateService.loadScript();
+  }
 }
